a548f1dcb9f5651b00a62ed5de44ed8d751a16ff
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-31 23:29:55 +0530

    updated

--- Components/Default Comps/Navbar.tsx
+++ Components/Default Comps/Navbar.tsx
@@ -1,15 +1,17 @@
 "use client";
-import DarkModeToggle from "./DarkModeToggle";
-import { useTheme } from "../Providers/ThemeProvider";
+// import DarkModeToggle from "./DarkModeToggle";
+// import { useTheme } from "../Providers/ThemeProvider";
+
 interface NavbarProps {}
 
 function Navbar({}: NavbarProps) {
-  const { darkMode, toggleDarkMode } = useTheme();
+  // const { darkMode, toggleDarkMode } = useTheme();
 
   return (
     <div className="mx-auto w-full fixed ">
       <h1>navbar</h1>
-      <DarkModeToggle darkMode={darkMode} onDarkModeToggle={toggleDarkMode} />
+      {/* <DarkModeToggle darkMode={darkMode} onDarkModeToggle={toggleDarkMode} />
+       */}
     </div>
   );
 }
