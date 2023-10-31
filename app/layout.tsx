a1be97772cf58b543afab80506697b2266505526
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-31 22:27:35 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/Components/Default Comps/Navbar";
+import Footer from "@/Components/Default Comps/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
