01b1d358822d873ee46d94e16647abf8d728c235
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-31 23:40:03 +0530

    updated

--- Components/Default Comps/Navbar.tsx
+++ Components/Default Comps/Navbar.tsx
@@ -1,16 +1,66 @@
 "use client";
-// import DarkModeToggle from "./DarkModeToggle";
-// import { useTheme } from "../Providers/ThemeProvider";
+import Link from "next/link";
+export const menuData = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Services",
+    path: "/services",
+    subMenu: [
+      { id: 21, title: "Service 1", path: "/services/service1" },
+      { id: 22, title: "Service 2", path: "/services/service2" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Departments",
+    path: "/departments",
+    subMenu: [
+      { id: 31, title: "Department 1", path: "/departments/department1" },
+      { id: 32, title: "Department 2", path: "/departments/department2" },
+    ],
+  },
+  {
+    id: 4,
+    title: "About",
+    path: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    path: "/contact",
+  },
+];
 
 interface NavbarProps {}
 
 function Navbar({}: NavbarProps) {
-  // const { darkMode, toggleDarkMode } = useTheme();
-
   return (
     <div className="mx-auto w-full fixed ">
-      <h1>navbar</h1>
-      {/* <DarkModeToggle darkMode={darkMode} onDarkModeToggle={toggleDarkMode} /> */}
+      <div className="container mx-auto">
+        <ul className="flex justify-between">
+          {menuData.map((item) => (
+            <li key={item.id} className="relative group flex">
+              <Link href={item.path}>
+                <a className="text-white py-2 px-4">{item.title}</a>
+              </Link>
+              {item.subMenu && (
+                <div className="absolute hidden mt-2 space-y-2 py-2 px-3 bg-white group-hover:block">
+                  {item.subMenu.map((subItem) => (
+                    <Link key={subItem.id} href={subItem.path}>
+                      <a className="text-gray-700">{subItem.title}</a>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ Components/Default Comps/testnav.tsx
@@ -0,0 +1,17 @@
+"use client";
+// import DarkModeToggle from "./DarkModeToggle";
+// import { useTheme } from "../Providers/ThemeProvider";
+
+interface NavbartestProps {}
+
+function Navbartest({}: NavbartestProps) {
+  // const { darkMode, toggleDarkMode } = useTheme();
+
+  return (
+    <div className="mx-auto w-full fixed ">
+      {/* <DarkModeToggle darkMode={darkMode} onDarkModeToggle={toggleDarkMode} /> */}
+    </div>
+  );
+}
+
+export default Navbartest;
