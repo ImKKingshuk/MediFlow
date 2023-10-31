8cfe63e5c9f4eff9c11e6e283f00dddf96c769f2
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-31 22:41:28 +0530

    updated

---
+++ Components/Providers/ThemeProvider.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+interface ThemeContextProps {
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
