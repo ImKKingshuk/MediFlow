42b7bbbe9404e0e62ad327e36a259342894f2a86
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-02 04:11:42 +0530

    updated

--- Components/Default Comps/Navbar.tsx
+++ Components/Default Comps/Navbar.tsx
@@ -1,80 +1,69 @@
+"use client";
 import Link from "next/link";
-import { FaBars } from "react-icons/fa";
-import { useState } from "react";
+const menuData = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Services",
+    path: "/services",
+    subMenu: [
+      { id: 21, title: "Service", path: "/services/service1" },
+      { id: 22, title: "Service", path: "/services/service2" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Departments",
+    path: "/departments",
+    subMenu: [
+      { id: 31, title: "Department", path: "/departments/department1" },
+      { id: 32, title: "Department", path: "/departments/department2" },
+    ],
+  },
+  {
+    id: 4,
+    title: "About",
+    path: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    path: "/contact",
+  },
+];
 
 interface NavbarProps {}
 
-function Navbar(): NavbarProps {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const handleToggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
+function Navbar({}: NavbarProps) {
   return (
-    <div className="w-full fixed 0 z-40">
-      <div className="backdrop-blur mx-auto">
+    <div className=" w-full fixed 0 z-40 ">
+      <div className="backdrop-blur mx-auto ">
         <ul className="flex justify-between text-black text-md p-4">
-          <li className="flex items-center">
-            <Link href="/">
-              <span className="py-2 px-4">Home</span>
-            </Link>
-          </li>
-          <li className="flex items-center">
-            <Link href="/services">
-              <span className="py-2 px-4">Services</span>
-            </Link>
-          </li>
-          <li className="flex items-center">
-            <Link href="/departments">
-              <span className="py-2 px-4">Departments</span>
-            </Link>
-          </li>
-          <li className="flex items-center">
-            <Link href="/about">
-              <span className="py-2 px-4">About</span>
-            </Link>
-          </li>
-          <li className="flex items-center">
-            <Link href="/contact">
-              <span className="py-2 px-4">Contact</span>
-            </Link>
-          </li>
-          <li className="flex items-center mobile-menu-button">
-            <FaBars onClick={handleToggleMobileMenu} />
-          </li>
+          {menuData.map((item) => (
+            <li key={item.id} className="relative group flex">
+              <Link href={item.path}>
+                <span className=" py-2 px-4">{item.title}</span>
+              </Link>
+              {item.subMenu && (
+                <ul className="absolute hidden top-12 w-fit space-y-2 rounded-lg py-2 px-3 bg-white shadow-3xl border-2 group-hover:block">
+                  {item.subMenu.map((subItem) => (
+                    <li key={subItem.id}>
+                      <Link href={subItem.path}>
+                        <span className="text-gray-700 py-2">
+                          {subItem.title}
+                        </span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
         </ul>
-        {isMobileMenuOpen && (
-          <div className="absolute top-12 left-0 right-0 bg-white shadow-3xl border-2">
-            <ul className="flex justify-between text-black text-md p-4">
-              <li className="flex items-center">
-                <Link href="/">
-                  <span className="py-2 px-4">Home</span>
-                </Link>
-              </li>
-              <li className="flex items-center">
-                <Link href="/services">
-                  <span className="py-2 px-4">Services</span>
-                </Link>
-              </li>
-              <li className="flex items-center">
-                <Link href="/departments">
-                  <span className="py-2 px-4">Departments</span>
-                </Link>
-              </li>
-              <li className="flex items-center">
-                <Link href="/about">
-                  <span className="py-2 px-4">About</span>
-                </Link>
-              </li>
-              <li className="flex items-center">
-                <Link href="/contact">
-                  <span className="py-2 px-4">Contact</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        )}
       </div>
     </div>
   );
