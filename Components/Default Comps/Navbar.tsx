b8dbbff7e55370afa8441e5df8e851addcb53211
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-02 03:37:18 +0530

    updated

--- Components/Default Comps/Navbar.tsx
+++ Components/Default Comps/Navbar.tsx
@@ -1,5 +1,7 @@
-"use client";
 import Link from "next/link";
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi"; // Import the icons
+
 const menuData = [
   {
     id: 1,
@@ -39,21 +41,60 @@ const menuData = [
 interface NavbarProps {}
 
 function Navbar({}: NavbarProps) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMobileMenuToggle = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <div className=" w-full fixed 0 z-40 ">
-      <div className="backdrop-blur mx-auto ">
-        <ul className="flex justify-between text-black text-md p-4">
+    <div className="w-full fixed top-0 z-40">
+      <div className="backdrop-blur mx-auto">
+        <div className="flex justify-between items-center text-black text-md p-4">
+          <Link href="/">
+            <span className="cursor-pointer">Logo</span>
+          </Link>
+          <div className="md:hidden">
+            {isMobileMenuOpen ? (
+              <FiX
+                className="h-6 w-6 cursor-pointer"
+                onClick={handleMobileMenuToggle}
+              />
+            ) : (
+              <FiMenu
+                className="h-6 w-6 cursor-pointer"
+                onClick={handleMobileMenuToggle}
+              />
+            )}
+          </div>
+        </div>
+        <ul
+          className={`flex md:space-x-4 ${
+            isMobileMenuOpen ? "flex-col" : "hidden"
+          }`}
+        >
           {menuData.map((item) => (
-            <li key={item.id} className="relative group flex">
+            <li
+              key={item.id}
+              className={`relative group ${
+                isMobileMenuOpen ? "w-full" : "w-fit"
+              }`}
+            >
               <Link href={item.path}>
-                <span className=" py-2 px-4">{item.title}</span>
+                <span className="py-2 px-4 cursor-pointer">{item.title}</span>
               </Link>
               {item.subMenu && (
-                <ul className="absolute hidden top-12 w-fit space-y-2 rounded-lg py-2 px-3 bg-white shadow-3xl border-2 group-hover:block">
+                <ul
+                  className={`absolute ${
+                    isMobileMenuOpen ? "block" : "hidden"
+                  } top-12 ${
+                    isMobileMenuOpen ? "w-full" : "w-fit"
+                  } space-y-2 rounded-lg py-2 px-3 bg-white shadow-3xl border-2`}
+                >
                   {item.subMenu.map((subItem) => (
                     <li key={subItem.id}>
                       <Link href={subItem.path}>
-                        <span className="text-gray-700 py-2">
+                        <span className="text-gray-700 py-2 cursor-pointer">
                           {subItem.title}
                         </span>
                       </Link>
