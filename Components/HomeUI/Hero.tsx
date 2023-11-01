8f5d4e300d3100229441384e247e52824de3dc3a
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-01 05:32:49 +0530

    updated

--- Components/HomeUI/Hero.tsx
+++ Components/HomeUI/Hero.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 
 interface HeroProps {}
@@ -5,35 +6,84 @@ interface HeroProps {}
 function Hero({}: HeroProps) {
   return (
     <div className="mx-auto">
-      <div className="relative bg-zinc-100">
-        <div className="grid grid-cols-1 md:grid-cols-2 md:gap-8">
-          <div className="p-4 md:p-0 flex flex-col justify-start items-start gap-6">
-            <h1 className="text-blue-950 text-4xl md:text-6xl font-semibold font-['Lexend'] capitalize">
-              The best place for <br /> your doctor <br /> Find now
-            </h1>
-            <p className="opacity-75 text-slate-500 text-base font-normal font-['Lexend'] leading-7">
+      <div className="w-[1280px] h-[832px] relative bg-zinc-100">
+        <div className="w-[451px] h-[440px] left-[756px] top-[127px] absolute">
+          <Image
+            src="/babydog.png"
+            alt="Image 1"
+            width={119}
+            height={121}
+            className="left-0 top-[60px] absolute rounded-3xl"
+          />
+          <Image
+            src="/babydog.png"
+            alt="Image 2"
+            width={119}
+            height={121}
+            className="left-[332px] top-[223px] absolute rounded-3xl"
+          />
+          <Image
+            src="/babydog.png"
+            alt="Image 3"
+            width={119}
+            height={121}
+            className="left-0 top-[223px] absolute rounded-3xl"
+          />
+          <Image
+            src="/babydog.png"
+            alt="Image 4"
+            width={119}
+            height={121}
+            className="left-[332px] top-[60px] absolute rounded-3xl"
+          />
+          <Image
+            src="/babydog.png"
+            alt="Image 5"
+            width={119}
+            height={121}
+            className="left-[172px] top-0 absolute rounded-3xl"
+          />
+          <Image
+            src="/babydog.png"
+            alt="Image 6"
+            width={119}
+            height={121}
+            className="left-[172px] top-[319px] absolute rounded-3xl"
+          />
+        </div>
+        <div className="left-[55px] top-[159px] absolute flex-col justify-start items-start gap-8 inline-flex">
+          <div className="flex-col justify-start items-start gap-6 flex">
+            <div className="w-[496px]">
+              <span className="text-blue-950 text-[40px] font-semibold font-['Lexend'] capitalize">
+                The best place for <br />
+                your{" "}
+              </span>
+              <span className="text-[40px] font-semibold font-['Lexend'] capitalize">
+                your doctor
+                <br />
+              </span>
+              <span className="text-blue-950 text-[40px] font-semibold font-['Lexend'] capitalize">
+                Find now
+              </span>
+            </div>
+            <div className="w-[496px] opacity-75 text-slate-500 text-base font-normal font-['Lexend'] leading-7">
               some more meow mwow meow
-            </p>
-            <div className="flex items-center gap-4">
-              <div className="w-6 h-6 relative" />
-              <input
-                type="text"
-                placeholder="Search for doctor/ biryani/ mutton chap/ chicken/ meow /meow"
-                className="w-full py-2 px-4 bg-white rounded-32px border border-zinc-200 text-neutral-400 text-base font-medium font-['Lexend'] leading-snug"
-              />
-              <button className="px-4 py-3 bg-emerald-500 rounded-32px flex items-center gap-1">
-                <span className="text-white text-sm font-semibold font-['Lexend'] leading-snug">
-                  Search
-                </span>
-                <div className="w-6 h-6 relative" />
-              </button>
             </div>
           </div>
-          <div className="hidden md:block relative">
-            <Image src="/babydog.png" alt="Image 1" width={119} height={121} />
+          <div className="w-[496px] pl-6 pr-1 py-1 bg-white rounded-[32px] border border-zinc-200 justify-start items-center gap-4 inline-flex">
+            <div className="w-6 h-6 relative" />
+            <div className="grow shrink basis-0 text-neutral-400 text-base font-medium font-['Lexend'] leading-snug">
+              Serarch for doctor/ biryani/ mutton chap/ chicken/ meow /meow
+            </div>
+            <div className="px-4 py-3 bg-emerald-500 rounded-[32px] justify-start items-center gap-1 flex">
+              <div className="text-white text-sm font-semibold font-['Lexend'] leading-snug">
+                Search
+              </div>
+              <div className="w-6 h-6 relative" />
+            </div>
           </div>
         </div>
-        <div className="text-4xl md:text-6xl font-semibold font-['Lexend'] capitalize absolute left-[31px] top-[32px]">
+        <div className="left-[31px] top-[32px] absolute text-[40px] font-semibold font-['Lexend'] capitalize">
           Meow Web
         </div>
       </div>
