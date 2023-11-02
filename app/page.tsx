af463f2172d7fcc8514867fa491dfdf294a8538f
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-02 10:27:06 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "@/Components/HomeUI/Hero";
+import DoctorDashboard from "@/Components/Dashboard/DoctorDashboard";
 export default function Home() {
   return (
-    <div className="mx-6">
+    <div className="mx-6 space-y-[8rem] ">
       <Hero />
+      <DoctorDashboard />
     </div>
   );
 }
